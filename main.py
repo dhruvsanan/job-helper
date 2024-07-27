940f694829d6dcd21f017bb226b01d40d1112fda
dhruv-sanan <dhruvsanan47@gmail.com>
2024-07-27 16:38:55 +0530

    Updated

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from st_copy_to_clipboard import st_copy_to_clipboard
 import google.generativeai as genai
 import streamlit as st
-genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+import os
+# genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+genai.configure(api_key=os.getenv("GOOGLE_API _KEY"))
 
 
 def get_gemini_response(a, b, c, d):
@@ -33,41 +35,76 @@ jt = st.text_input("Job Title: ", key="jt")
 jd = st.text_area("Job Description: ", key="jd")
 ques = st.text_input("Question: ", key="ques")
 
-col4, col5, col6 = st.columns(3)
+on = st.toggle("Copy details")
 
-with col4:
-    lin = st.button("Linkedin")
+if on:
+    col4, col5, col6, col8 = st.columns(4)
 
-with col5:
-    per = st.button("Personal")
+    with col4:
+        lin = st.button("Linkedin")
 
-with col6:
-    git = st.button("Github")
+    with col5:
+        per = st.button("Personal")
 
+    with col6:
+        git = st.button("Github")
+    with col8:
+        email_template = st.button("Email Template")
 
-col1, col2, col3 = st.columns(3)
+    if lin:
+        st_copy_to_clipboard("https://www.linkedin.com/in/dhruvsanan01/")
+        st.success('Text copied successfully!')
 
-with col1:
-    email = st.button("Email")
+    if per:
+        st_copy_to_clipboard("https://dhruvsanan.vercel.app")
+        st.success('Text copied successfully!')
 
-with col2:
-    cl = st.button("Cover Letter")
+    if git:
+        st_copy_to_clipboard("https://github.com/dhruvsanan")
+        st.success('Text copied successfully!')
+    if email_template:
+        st_copy_to_clipboard("""Dear Colby and Nisar,
 
-with col3:
-    q = st.button("Q&A")
+I'm writing to express my strong interest in the Chief Technology Officer and Co-Founder position at ReFocus AI. Your mission to leverage AI for improving customer retention in the insurance industry deeply resonates with me. My background in both AI development and building full-stack SaaS applications makes me a perfect fit to lead your engineering team and contribute to ReFocus AI's growth.
 
+In my previous role at Nvision Soft (Miami, Florida), I spearheaded the development of a smart Applicant Tracking System (ATS) that reduced hiring time by 60%. This project highlights my ability to deliver data-driven solutions that significantly impact business goals. 
 
-if lin:
-    st_copy_to_clipboard("https://www.linkedin.com/in/dhruvsanan01/")
-    st.success('Text copied successfully!')
+I'm also eager to highlight my personal projects that demonstrate my understanding of LLMs:
 
-if per:
-    st_copy_to_clipboard("https://dhruvsanan.vercel.app")
-    st.success('Text copied successfully!')
+AI Lead Magnet (SaaS Product): I spearheaded the development of a full-stack AI application that transforms content into interactive AI lead magnets. This project involved leveraging Langchain models to create a dynamic and engaging user experience.
+ChatPDF (SaaS Product): I built a SaaS platform utilizing Next.js and OpenAI's capabilities. This platform allows users to upload PDFs for proprietary data training and efficient information extraction through an LLM model. My work focused on optimizing the LLM integration for seamless data processing and user experience.
+Video Analyser project: I pushed the boundaries of AI capabilities by developing a video analysis tool before Gemini Pro could accept video inputs. This project utilized cutting-edge techniques:
+Computer Vision: I leveraged computer vision libraries to extract individual frames from the video stream.
+OpenAI's Whisper Model API: I integrated OpenAI's Whisper AI's API to analyse the spoken dialogue. This allowed me to analyze the video content based on both the visual information and the spoken dialogue.
+This project demonstrates my ability to not only develop AI solutions but also translate them into user-centric applications that address real-world problems – a skillset directly applicable to ReFocus AI's mission.
 
-if git:
-    st_copy_to_clipboard("https://github.com/dhruvsanan")
-    st.success('Text copied successfully!')
+Having thrived in a remote environment at Nvision Soft (Miami, Florida), I'm confident in seamlessly integrating into your team and collaborating effectively across time zones. My strong communication skills and ability to explain complex technical concepts ensure clear collaboration within the team and with external partners.
+
+I'm eager to discuss how my expertise in AI development, full-stack development, and a passion for building impactful solutions can contribute to ReFocus AI's success. I'm available for a call at your earliest convenience.
+
+Thank you for your time and consideration. I've included a link to my calendar for scheduling convenience: 
+
+Calendar
+
+Sincerely,
+
+Dhruv Sanan
++91-7901919447 |  Personal Website""")
+        st.success('Text copied successfully!')
+else:
+    col1, col2, col3, col7 = st.columns(4)
+
+    with col1:
+        email = st.button("Email")
+
+    with col2:
+        cl = st.button("Cover Letter")
+
+    with col3:
+        q = st.button("Q&A")
+
+    with col7:
+        qa = st.button("Resume Q&A")
 
 
 # clear_button = st.button("Clear Results")
@@ -79,11 +116,29 @@ if git:
 if 'q' not in st.session_state:
     st.session_state['q'] = []
 
+if 'qa' not in st.session_state:
+    st.session_state['qa'] = []
+
 if 'cl' not in st.session_state:
     st.session_state['cl'] = []
 
 if 'email' not in st.session_state:
     st.session_state['email'] = []
+
+show_answer = st.button("Show Answer")
+if show_answer:
+    if 'q' in st.session_state:
+        st.write(q)
+
+    if 'qa' in st.session_state:
+        st.write(qa)
+
+    if 'cl' in st.session_state:
+        st.write(cl)
+
+    if 'email' not in st.session_state:
+        st.write(email)
+
 email_template = """Dear Colby and Nisar,
 
 I'm writing to express my strong interest in the Chief Technology Officer and Co-Founder position at ReFocus AI. Your mission to leverage AI for improving customer retention in the insurance industry deeply resonates with me. My background in both AI development and building full-stack SaaS applications makes me a perfect fit to lead your engineering team and contribute to ReFocus AI's growth.
@@ -113,7 +168,7 @@ Dhruv Sanan
 +91-7901919447 |  Personal Website"""
 email_prompt = """ You are an expert in creating professional emails
                 I have provided you with resume and jd. 
-                create an email to founder explaining why are you a good fit for {jt} position. 
+                create an email to founder explaining why are you a good fit for given position. 
                 the first few sentences should explain why are you a great fit for the job, 
                 the second part should be an interesting part about you that related to the job so that they can see you know what you're talking about. 
                 then you should talk about your previous experience at NvisionSoft where you Automated resume review, enabling the processing of over 150 resumes per hour. Applied advanced relevance techniques to shortlist the top 10% of candidates, ensuring high-quality hires and improved talent acquisition outcomes through cross-functional teamwork.
@@ -121,8 +176,7 @@ email_prompt = """ You are an expert in creating professional emails
                 Also give them confidence and you have already worked with a company in US so time difference isn't really a problem for you. 
                 Also, you are willing to relocate if necessary.
                 make sure that the email is interesting and eye catching, demonstrating your skills and experience and how can you contribute to the company if given the job.
-                make sure to follow the below email template
-                email template- {email_template}
+                make sure to follow the follwing template
                 """
 cl_prompt = """create a cover letter for this job using below format 
                 Greetings to the hiring team
@@ -148,6 +202,8 @@ cl_prompt = """create a cover letter for this job using below format
                 Make sure to talk about your previous experience at NvisionSoft where you Automated resume review, enabling the processing of over 150 resumes per hour. Applied advanced relevance techniques to shortlist the top 10% of candidates, ensuring high-quality hires and improved talent acquisition outcomes through cross-functional teamwork.
                 """
 q_prompt = """Using the Jd and resume, Answer the question in an interesting and eye catching way demonstrating your skills and experience and how can you contribute to the company if given the job."""
+qa_prompt = """Using resume, Answer the question in an interesting and eye catching way demonstrating your skills and experience and how can you contribute to the company if given the job. Your answer should reflect your personality."""
+
 if email:
     if jt and jd:
         email = get_gemini_response(pdf_content, jt, jd, email_prompt)
@@ -169,3 +225,8 @@ if q:
         st.write(q)
     else:
         st.write("Please write JT and JD")
+if qa:
+    model = genai.GenerativeModel('gemini-1.5-pro-latest')
+    qa = model.generate_content([pdf_content, qa_prompt+ques])
+    st.session_state['qa'] = qa
+    st.write(qa)
